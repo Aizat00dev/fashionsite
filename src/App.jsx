73fb4aabab8f59d9be75213dc4a7b46bfd4b199b
Aizat00dev <urmanbetowwa0@gmail.com>
2024-08-17 06:05:35 +0600

    commit to file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src="./images/im1.png" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default[
+    {   
+        id: 1,
+       
+        title: 'IGURE',
+        subtitle: 'green muscle fit polo shirt',
+        price: {
+            old: 229,
+            new: 129,
+        }
+    },
+    {
+        id:2,
+        title: 'AVONE',
+        subtitle: 'black muscle fit polo shirt',
+        price: {
+            old: 585,
+            new: 429,
+        }
+    },
+    {   id:3,
+        title: 'DRESS',
+        subtitle: 'red muscle fit polo shirt',
+        price: {
+            old: 456,
+            new: 392,
+        }
+    },
+    {   id:4,
+        title: 'TACHEN',
+        subtitle: 'skyblue muscle fit polo shirt',
+        price: {
+            old: 329,
+            new: 229,
+        }
+    },
+]
